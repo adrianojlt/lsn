88ff3474c625c1f54fb945c5e79f064026c8b3c5
adrianojlt <adrianojlt@gmail.com>
2026-03-11 17:50:33 +0000

    first commit

---
+++ src/main/java/com/lsn/Application.java
@@ -0,0 +1,22 @@
+package com.lsn;
+
+import java.util.Scanner;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNextLine()) {
+
+            String line = scanner.nextLine();
+
+            if ("exit".equals(line)) {
+                break;
+            }
+
+            System.out.println(line);
+        }
+    }
+}
